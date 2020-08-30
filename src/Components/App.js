261f2a14620e8db243fde9096fb434fbccacb6a6
MCris29 <crismax0629@gmail.com>
2020-08-30 11:42:14 -0500

    Layout

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,16 +5,34 @@ import CardInformation from '../Components/CardInformation';
 import FontSlider from '../Components/FontSlider';
 import Item from "../Components/Item";
 import Nav from "./Nav";
-function App() {
+import {Layout, Menu, Breadcrumb} from 'antd';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+
+const {Header, Content,} = Layout;
+
+const App = () => {
 
     return (
-        <>
-            <Nav />
-            <FontSlider/>
-            <CardInformation />
-            <Footer/>
+        <Router>
 
-        </>
+            <Layout className="layout">
+                <Header>
+                    <Nav/>
+                </Header>
+                <Content style={{padding: '0 50px'}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <FontSlider/>
+                    <CardInformation/>
+                </Content>
+                <Footer/>
+            </Layout>
+
+        </Router>
     );
 }
 
