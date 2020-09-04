b5c2e5093fa17da2f233eb9f88f8427221a57ebd
MCris29 <crismax0629@gmail.com>
2020-09-03 19:44:31 -0500

    Home Responsive

--- src/Components/FontSlider.js
+++ src/Components/FontSlider.js
@@ -1,40 +1,47 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Carousel from "react-elastic-carousel";
 import Item from "../Components/Item";
 import "../Styles/styles.css";
-import { Row ,Col} from "antd";
+import {Row, Col, Carousel} from "antd";
 
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  //{ width: 550, itemsToShow: 2 },
-  //{ width: 768, itemsToShow: 3 },
-  //{ width: 1200, itemsToShow: 4 },
-];
 
-function FontSlider(){
-  return (
-    <>
-    <Row>
-      <Col span={24}>
-    <div className="slider slider-font"> 
-    
-      <div className="main">
-      <Carousel breakPoints={breakPoints}>
-          <Item><div className="fondo-imagen1"><h6 className="title-image">fotografia : Fernando zhiminaicela</h6></div></Item>
-          <Item><div className="fondo-imagen2"><h6 className="title-image">fotografia : 472619</h6></div></Item>
-          <Item><div className="fondo-imagen3"><h6 className="title-image">fotografia : Alejomiranda</h6></div></Item>
-          <Item><div className="fondo-imagen4"><h6 className="title-image">fotografia : Mario Scheammborn</h6></div></Item>
-          <Item><div className="fondo-imagen5"><h6 className="title-image">fotografia : Marisa04</h6></div></Item>
-          <Item><div className="fondo-imagen6"><h6 className="title-image">fotografia : PublicDomainPicture</h6></div></Item>
-          
-        </Carousel>
-      </div>
-      </div>
-      </Col>
-      </Row>
-    </>
-  );
+function FontSlider() {
+    return (
+        <>
+            <Carousel autoplay>
+                <Item>
+                    <div className="fondo-imagen1 config-image">
+                        <h6 className="title-image">fotografia : Fernando zhiminaicela</h6>
+                    </div>
+                </Item>
+                <Item>
+                    <div className="fondo-imagen2 config-image">
+                        <h6 className="title-image">fotografia: 472619</h6>
+                    </div>
+                </Item>
+                <Item>
+                    <div className="fondo-imagen3 config-image">
+                        <h6 className="title-image">fotografia: Alejomiranda</h6>
+                    </div>
+                </Item>
+                <Item>
+                    <div className="fondo-imagen4 config-image">
+                        <h6 className="title-image">fotografia: Mario Scheammborn</h6>
+                    </div>
+                </Item>
+                <Item>
+                    <div className="fondo-imagen5 config-image">
+                        <h6 className="title-image">fotografia: Marisa04</h6>
+                    </div>
+                </Item>
+                <Item>
+                    <div className="fondo-imagen6 config-image">
+                        <h6 className="title-image">fotografia: PublicDomainPicture</h6>
+                    </div>
+                </Item>
+            </Carousel>
+        </>
+    );
 }
+
 export default FontSlider;
 
