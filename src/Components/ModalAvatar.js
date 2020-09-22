23892b01596c31ef04dc857318cd6b628b1a47af
MCris29 <crismax0629@gmail.com>
2020-09-22 13:12:52 -0500

    Arreglo Modal Avatar

--- src/Components/ModalAvatar.js
+++ src/Components/ModalAvatar.js
@@ -2,27 +2,28 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import '../Styles/ModalAvatar.scss';
 import '../Styles/App.css';
-import {  Row ,Col} from "antd";
+import {Row, Col} from "antd";
 
 const ModalAvatar = (props) => {
     return (
         <div>
             <div className="moda-wrapper "
-                style={{
-                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
+                 style={{
+                     transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                     opacity: props.show ? '1' : '0',
+                     width: 'fit-content'
+                 }}>
                 <div className="modal-header">
                     <Row>
                         <Col span={22}>
-                        <h3 >Conoce Ecuador de una forma</h3>    
-                        </Col >
-                        <Col span={2}>
-                        
-                        <span className="close-modal-btn" onClick={props.close}><div className="container-close">X</div></span>
-                        
                         </Col>
-                    </Row>                 
+                        <Col span={2}>
+
+                            <span className="close-modal-btn" onClick={props.close}><div
+                                className="container-close">X</div></span>
+
+                        </Col>
+                    </Row>
                 </div>
                 <div className="modal-body">
                     <div>
